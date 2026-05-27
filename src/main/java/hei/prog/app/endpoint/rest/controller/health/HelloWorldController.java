d45c11f0795f3c0c8f8b4420eb8435c3a74bb8ac
Hiraina-RND <hei.hiraina@gmail.com>
2026-05-27 17:37:26 +0300

    feat: add hello world endpoint

---
+++ src/main/java/hei/prog/app/endpoint/rest/controller/health/HelloWorldController.java
@@ -0,0 +1,19 @@
+package hei.prog.app.endpoint.rest.controller.health;
+
+import hei.prog.app.service.HelloWorldService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class HelloWorldController {
+    HelloWorldService helloWorldService;
+
+    @GetMapping("/hello")
+    ResponseEntity<String> hello() {
+        return new ResponseEntity<>(helloWorldService.sayHello(), HttpStatusCode.valueOf(200));
+    }
+}
